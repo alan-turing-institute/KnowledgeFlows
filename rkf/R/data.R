797e7f3c8d20a452568ee6a20b70437df577b6f4
crangelsmith <camila.rangel.smith@gmail.com>
2019-03-26 14:49:37 +0000

    uploading synthetic data

---
+++ rkf/R/data.R
@@ -0,0 +1,17 @@
+#' Synthetic ASHE dataset
+#'
+#' Sample generated from expected columns on the ASHE dataset, contains all missing values except the
+#' sic03 and sic07 variables containing the industrial classification.
+#'
+#' @format A data frame with 1301 rows and 133 variables.
+#' @source Synthetic generated
+"ashe_sic_5dig"
+
+#' SIC code dataset for 2003 and 2007 (5 digit)
+#'
+#' Current Standard Industrial Classification (SIC) used in classifying business
+#' establishments and other statistical units by the type of economic activity in which they are engaged.
+#'
+#' @format A data frame with 1301 rows and 5 variables.
+#' @source \url{http://www.ons.gov.uk/ons/guide-method/classifications/current-standard-classifications/standard-industrial-classification/correlation-between-uk-sic-2003-and-2007.xls}
+"sic03_07_5dig"
