1b7357872855362380de3965ba343374995181fb
Tim Hobson <thobson@turing.ac.uk>
2019-03-26 15:19:37 +0000

    Add empty_transistion_matrix function

---
+++ rkf/R/empty_transition_matrix.R
@@ -0,0 +1,22 @@
+#' Create an empty worker transition matrix
+#'
+#' TODO: description
+#'
+#' @param data
+#' A data frame.
+#' @param colname
+#' The name of the industry code column in the data.
+#'
+#' @return
+#' An empty matrix.
+#'
+#' @examples
+#' \dontrun{
+#' empty_transition_matrix(ashe_sic_5dig, colname = "sic07")
+#' }
+#' @import httr
+#' @export
+empty_transition_matrix <- function(data, colname) {
+
+  # data[[colname]]
+}
