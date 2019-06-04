87107ee5774057c1fe6f008114a4eed7359a5ad3
crangelsmith <camila.rangel.smith@gmail.com>
2019-06-04 15:26:45 +0100

    refactoring

--- rkf/R/create_and_fill_industry_transition_matrix.R
+++ rkf/R/create_and_fill_industry_transition_matrix.R
@@ -9,29 +9,28 @@
 #'
 #' @examples
 #' \dontrun{
-#' create_and_fill_industry_transition_matrix <- function(sample, cuts, cutsString,indClass)
+#' create_and_fill_industry_transition_matrix <- function(sample, cuts, cutsName,indClass)
 #' }
 #' @import httr
 #' @export
-create_and_fill_industry_transition_matrix <- function(sample, cuts, cutsString, indClass) {
+create_and_fill_industry_transition_matrix <- function(sample, cuts, cuts_name, ind_class) {
 
   # make cuts on inital sample
-  selected_sample <- selection_cuts_ashe(sample, cuts, cutsString)
+  selected_sample <- selection_cuts_ashe(sample, cuts, cuts_name)
 
   print('test')
   # get all workers on selected sample
   unique_workers <- unique(selected_sample$piden)
 
   # create initial empty transition matrix
-  transition_matrix <- empty_industry_transition_counts_matrix(selected_sample, colname = indClass)
+  transition_matrix <- empty_industry_transition_counts_matrix(selected_sample, ind_class)
 
 
   # loop by workers
   for (worker in unique_workers) {
 
-
     # create the transition matrix for a particular worker
-    trans_matrix_worker <- worker_transition_matrix(selected_sample, piden = worker, colname=indClass)
+    trans_matrix_worker <- worker_transition_matrix(selected_sample, piden = worker, ind_class)
 
     # fill  matrix with transitions from the worker
     transition_matrix <- fill_worker_transition_matrix(transition_matrix,trans_matrix_worker)
--- rkf/tests/testthat/test-run_ashe_sample_summary.R
+++ rkf/tests/testthat/test-run_ashe_sample_summary.R
@@ -3,7 +3,7 @@ context("test_run_ashe_sample_summary function")
 
 test_that("the run_ashe_sample_summary function works", {
 
-  result <- run_ashe_sample_summary(ashe_sic_5dig,'log/log_test_ashe_ashe_sic_5dig',"nocuts")
+  result <- run_ashe_sample_summary(ashe_sic_5dig,'log_test_ashe_ashe_sic_5dig',"nocuts")
 
   expect_equal(file.exists(result), expected = TRUE)
 
