d98c57a5b82e61b4fe04edca079886a7fcb4497d
crangelsmith <camila.rangel.smith@gmail.com>
2019-06-11 16:57:16 +0100

    adding missing flows in the reduced matrix

--- rkf/R/create_reduced_transition_matrix.R
+++ rkf/R/create_reduced_transition_matrix.R
@@ -25,6 +25,9 @@ create_reduced_transition_matrix <- function(transitionmatrix, min_counts) {
 
   c <- 1
 
+  # count remaining flows
+  missing_flows <- 0
+
   # start looping on the rows (starting industry in a flow)
   for(row in 1:length(trans_matrix_rows)) {
 
@@ -37,6 +40,10 @@ create_reduced_transition_matrix <- function(transitionmatrix, min_counts) {
 
     # apply minimum cut
     if (transitionmatrix[row_name,col_name]<min_counts){
+
+      # keep track of flows that could not be reported due to min counts
+      missing_flows <- missing_flows+ transitionmatrix[row_name,col_name]
+
       next()
 
     }
@@ -50,6 +57,12 @@ create_reduced_transition_matrix <- function(transitionmatrix, min_counts) {
 
   }
   }
+
+  # fill new matrix with the flows and counts
+  reduced_matrix[c,1] <- "0000"
+  reduced_matrix[c,2] <- "0000"
+  reduced_matrix[c,3] <- missing_flows
+
 
   # return reduced matrix
   return (reduced_matrix)
