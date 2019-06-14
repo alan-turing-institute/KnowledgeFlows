ef12717e59169c922c33c89cf1d5f92583ac5737
crangelsmith <camila.rangel.smith@gmail.com>
2019-06-14 15:39:23 +0100

    updating test for matrix expanding function

--- rkf/tests/testthat/test-fill_industry_transition_matrix_from_reduced.R
+++ rkf/tests/testthat/test-fill_industry_transition_matrix_from_reduced.R
@@ -3,17 +3,21 @@ context("test_fill_industry_transition_matrix_from_reduced function")
 
 test_that("the fill_industry_transition_matrix_from_reduced function works", {
 
-
-  sub_ashe_sample <- ashe_sic_5dig
+  sub_ashe_sample <- ashe_sic_5dig[1:1000,]
 
   transitionmatrix <- create_and_fill_industry_transition_matrix(sub_ashe_sample,(sub_ashe_sample$piden>0),"sub_ashe_sample$piden>0","sic07")
 
-  min_counts <- 5
+  min_counts <- 10
   reduced_matrix <- create_reduced_transition_matrix(transitionmatrix,min_counts)
 
-  expanded_matrix <- fill_industry_transition_matrix_from_reduced(reduced_matrix,sic03_07_5dig,"sic07")
+  expanded_matrix <- fill_industry_transition_matrix_from_reduced(reduced_matrix,sic03_07_5dig,"SIC.2007_CODE")
 
+  # make sure it is a matrix
+  expect_true(is.matrix(expanded_matrix))
 
-  expect_equal(expanded_matrix,transitionmatrix)
+  index_initial <- which(transitionmatrix>=(min_counts), arr.ind = TRUE)
+  index_expanded <- which(expanded_matrix>=(min_counts), arr.ind = TRUE)
+
+  expect_equal(index_initial,index_expanded)
 
 })
