7a9a2fe170bd2878f986ca20dda9dd51fc804c3a
crangelsmith <camila.rangel.smith@gmail.com>
2019-10-14 13:30:38 +0100

    adding modularity to jupyter notebook

--- python/labour_flows_matrix/src/networkx_functions.py
+++ python/labour_flows_matrix/src/networkx_functions.py
@@ -35,3 +35,71 @@ def build_networx_network(input_dataframe,label):
     return convert_igraph_to_networkx(network_igraph)
 
 
+def build_networx_from_df(input_df):
+
+
+    # In case you graph is undirected
+    network_nx = netx.Graph()
+
+    # loop over each node and add it to the graph, and include extra information
+    # loop over each edge and add it to the graph, and include extra information
+    for edge in range(0,input_df.shape[0]):
+
+        v1 = input_df.iloc[edge]['StartIndst']
+        v2 = input_df.iloc[edge]['FinalIndst']
+        weight = input_df.iloc[edge]['weight']
+
+        network_nx.add_edge(v1, v2, weight=weight)
+
+
+    return network_nx
+
+def include_mstree_information(input_dataframe,edges_list,weight_threshold):
+
+    is_mstree = []
+    is_mtree_or_weight = []
+
+    for i in range(0, input_dataframe.shape[0]):
+        source = input_dataframe.iloc[i]['StartIndst']
+        target = input_dataframe.iloc[i]['FinalIndst']
+        weight = input_dataframe.iloc[i]['weight']
+
+        mtree = 0
+        if (source, target) in edges_list:
+            mtree = 1
+
+        elif (target, source) in edges_list:
+            mtree = 1
+
+
+        is_mstree.append(mtree)
+
+        if (is_mstree == 1 or weight> weight_threshold):
+            is_mtree_or_weight.append(weight)
+        else:
+            is_mtree_or_weight.append(0)
+
+
+    input_dataframe['is_mstree'] = is_mstree
+    input_dataframe['is_mtree_or_weight'] = is_mtree_or_weight
+
+
+    return input_dataframe
+
+def prepare_dataframe_for_gephi(input_dataframe, include_mst=False):
+    input_dataframe['Source'] = input_dataframe['StartIndst']
+    input_dataframe['Target'] = input_dataframe['FinalIndst']
+    input_dataframe['Weight'] = input_dataframe['weight']
+    input_dataframe['mst_Weight'] = input_dataframe['is_mstree'] * input_dataframe[
+        'weight']
+
+    input_dataframe = input_dataframe[input_dataframe['is_mtree_or_weight']!= 0.0]
+
+    if include_mst:
+
+        output = input_dataframe[['Source', 'Target', 'Weight', 'mst_Weight', 'is_mstree','is_mtree_or_weight']]
+
+    else:
+        output = input_dataframe[['Source', 'Target', 'Weight']]
+
+    return output
